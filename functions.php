a37f7d7423b9a59c1f53154b49bff3d040a9b103
Chandrika Guntur <cguntur@gmail.com>
2019-03-18 11:50:51 -0700

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,29 @@
+//Custom blocks using ACF
+add_action('acf/init', 'my_acf_init');
+function my_acf_init() {
+
+	// check function exists
+	if( function_exists('acf_register_block') ) {
+
+    acf_register_block(array(
+      'name'  => 'featured-business',
+      'title' => __('Featured Business'),
+      'description' => __('A block that displays the selected business.'),
+      'render_callback' => 'my_acf_block_render_callback',
+      'category'  => 'formatting',
+      'icon'  => 'admin-comments',
+      'keywords'  => array('custom post type', 'custom post'),
+    ));
+	}
+}
+
+function my_acf_block_render_callback( $block ) {
+
+	// convert name ("acf/featured-business") into path friendly slug ("featured-business")
+	$slug = str_replace('acf/', '', $block['name']);
+
+	// include a template part from within the "template-parts/block" folder
+	if( file_exists( get_theme_file_path("/template-parts/block/content-{$slug}.php") ) ) {
+		include( get_theme_file_path("/template-parts/block/content-{$slug}.php") );
+	}
+}
